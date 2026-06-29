75177bd3a52fb09650f35742a485401cf723a315
Carlos Henrique Rabelo de Oliveira <carlos.oliveira@complyadvantage.com>
2026-06-29 16:36:56 +0100

    github actions

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,18 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/dances/",
   plugins: [tailwindcss(), reactRouter()],
   resolve: {
     tsconfigPaths: true,
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://calendar.google.com/calendar/",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
   },
 });
